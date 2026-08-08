5d88b2021f052d464156f7ac1778da7da8a9d0b2
Mrcomander <totoroirumon.1110456@gmail.com>
2026-08-08 23:06:59 +0900

    GameManagerの実装

---
+++ dokomiterunoyo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] int MainLoveMeter = 1000;
+    [SerializeField] int MainHp = 100;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
